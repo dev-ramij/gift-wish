41194113ac0a0370f014bc33e1813bd614d9e3ae
dev-ramij <ramij@ideavat.com>
2020-04-21 15:00:48 +0530

    Initialize

---
+++ source/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+function App(){
+  <h1>Test</h1>
+}
+
